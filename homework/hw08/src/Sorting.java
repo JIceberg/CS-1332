5380d2c1142332e91bc7fc2492ba9110a9b224de
JIceberg <jisenberg1050@gmail.com>
2020-10-20 18:08:27 -0400

    homework 8

---
+++ homework/hw08/src/Sorting.java
@@ -0,0 +1,276 @@
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ * Your implementation of various sorting algorithms.
+ *
+ * @author YOUR NAME HERE
+ * @version 1.0
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+
+public class Sorting {
+
+    /**
+     * Implement insertion sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of:
+     * O(n^2)
+     *
+     * And a best case running time of:
+     * O(n)
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     * @throws java.lang.IllegalArgumentException if the array or comparator is
+     *                                            null
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Arguments must not be null/");
+        }
+        for (int j = 1; j < arr.length; j++) {
+            int i = j;
+            while (i > 0 && comparator.compare(arr[i - 1], arr[i]) > 0) {
+                T temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+                i--;
+            }
+        }
+    }
+
+    /**
+     * Implement cocktail sort.
+     *
+     * It should be:
+     * in-place
+     * stable
+     * adaptive
+     *
+     * Have a worst case running time of:
+     * O(n^2)
+     *
+     * And a best case running time of:
+     * O(n)
+     *
+     * NOTE: See pdf for last swapped optimization for cocktail sort. You
+     * MUST implement cocktail sort with this optimization
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     * @throws java.lang.IllegalArgumentException if the array or comparator is
+     *                                            null
+     */
+    public static <T> void cocktailSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Arguments must not be null/");
+        }
+        boolean swapsMade = true;
+        int start = 0;
+        int end = arr.length - 1;
+        while (swapsMade) {
+            swapsMade = false;
+            int startIndex = start;
+            int endIndex = end;
+            for (int i = start; i < end; i++) {
+                if (comparator.compare(arr[i], arr[i + 1]) > 0) {
+                    T temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapsMade = true;
+                    endIndex = i;
+                }
+            }
+            end = endIndex;
+            if (swapsMade) {
+                swapsMade = false;
+                for (int i = end; i > start; i--) {
+                    if (comparator.compare(arr[i - 1], arr[i]) > 0) {
+                        T temp = arr[i];
+                        arr[i] = arr[i - 1];
+                        arr[i - 1] = temp;
+                        swapsMade = true;
+                        startIndex = i;
+                    }
+                }
+                start = startIndex;
+            }
+        }
+    }
+
+    /**
+     * Implement merge sort.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(n log n)
+     *
+     * And a best case running time of:
+     * O(n log n)
+     *
+     * You can create more arrays to run merge sort, but at the end, everything
+     * should be merged back into the original T[] which was passed in.
+     *
+     * When splitting the array, if there is an odd number of elements, put the
+     * extra data on the right side.
+     *
+     * Hint: If two data are equal when merging, think about which subarray
+     * you should pull from first
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array to be sorted
+     * @param comparator the Comparator used to compare the data in arr
+     * @throws java.lang.IllegalArgumentException if the array or comparator is
+     *                                            null
+     */
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Arguments must not be null/");
+        }
+
+        // base case
+        if (arr.length < 2) {
+            return;
+        }
+
+        // create the left and right subarrays
+        int length = arr.length;
+        int mid = length / 2;
+        T[] L = (T[]) new Object[mid];
+        T[] R = (T[]) new Object[length - mid];
+        for (int i = 0; i < mid; i++) {
+            L[i] = arr[i];
+        }
+        for (int i = mid; i < length; i++) {
+            R[i - mid] = arr[i];
+        }
+        mergeSort(L, comparator);
+        mergeSort(R, comparator);
+
+        // merge the two arrays
+        int i = 0;
+        int j = 0;
+        while (i < L.length && j < R.length) {
+            if (comparator.compare(R[j], L[i]) > 0) {
+                arr[i + j] = L[i];
+                i++;
+            } else {
+                arr[i + j] = R[j];
+                j++;
+            }
+        }
+
+        // copy over the rest of the array if needed
+        while (i < L.length) {
+            arr[i + j] = L[i];
+            i++;
+        }
+        while (j < R.length) {
+            arr[i + j] = R[j];
+            j++;
+        }
+    }
+
+    /**
+     * Implement LSD (least significant digit) radix sort.
+     *
+     * Make sure you code the algorithm as you have been taught it in class.
+     * There are several versions of this algorithm and you may not get full
+     * credit if you do not implement the one we have taught you!
+     *
+     * Remember you CANNOT convert the ints to strings at any point in your
+     * code! Doing so may result in a 0 for the implementation.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(kn)
+     *
+     * And a best case running time of:
+     * O(kn)
+     *
+     * You are allowed to make an initial O(n) passthrough of the array to
+     * determine the number of iterations you need.
+     *
+     * At no point should you find yourself needing a way to exponentiate a
+     * number; any such method would be non-O(1). Think about how how you can
+     * get each power of BASE naturally and efficiently as the algorithm
+     * progresses through each digit.
+     *
+     * Refer to the PDF for more information on LSD Radix Sort.
+     *
+     * You may use ArrayList or LinkedList if you wish, but it may only be
+     * used inside radix sort and any radix sort helpers. Do NOT use these
+     * classes with other sorts. However, be sure the List implementation you
+     * choose allows for stability while being as efficient as possible.
+     *
+     * Do NOT use anything from the Math class except Math.abs().
+     *
+     * @param arr the array to be sorted
+     * @throws java.lang.IllegalArgumentException if the array is null
+     */
+    public static void lsdRadixSort(int[] arr) {
+
+    }
+
+    /**
+     * Implement quick sort.
+     *
+     * Use the provided random object to select your pivots. For example if you
+     * need a pivot between a (inclusive) and b (exclusive) where b > a, use
+     * the following code:
+     *
+     * int pivotIndex = rand.nextInt(b - a) + a;
+     *
+     * If your recursion uses an inclusive b instead of an exclusive one,
+     * the formula changes by adding 1 to the nextInt() call:
+     *
+     * int pivotIndex = rand.nextInt(b - a + 1) + a;
+     *
+     * It should be:
+     * in-place
+     * unstable
+     * not adaptive
+     *
+     * Have a worst case running time of:
+     * O(n^2)
+     *
+     * And a best case running time of:
+     * O(n log n)
+     *
+     * Make sure you code the algorithm as you have been taught it in class.
+     * There are several versions of this algorithm and you may not receive
+     * credit if you do not use the one we have taught you!
+     *
+     * @param <T>        data type to sort
+     * @param arr        the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     * @param rand       the Random object used to select pivots
+     * @throws java.lang.IllegalArgumentException if the array or comparator or
+     *                                            rand is null
+     */
+    public static <T> void quickSort(T[] arr, Comparator<T> comparator,
+                                     Random rand) {
+
+    }
+}
